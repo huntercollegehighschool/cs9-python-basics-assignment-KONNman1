1469d6e49935c69e4c8192d74c9ff95c8a9f0e02
Kai Nizhner <103528458+KONNman1@users.noreply.github.com>
2022-04-17 20:06:20 +0000

    Finished the rest of the assignments (parts 3 - 5).

--- part4.py
+++ part4.py
@@ -2,7 +2,7 @@
 ______
 PART 4
 ______
-Write a program that prompts the user to enter two integer inputs. Those two number will be the base and height of a triangle. 
+Write a program that prompts the user to enter two integer inputs. Those two numbers will be the base and height of a triangle. 
 The program will then output the area of that triangle. (Reminder: the area of a triangle can be calculated by (base * height)/2 ).
 
 What should appear on the console when this code runs:
@@ -13,4 +13,9 @@ The area of the triangle is 12.0
 
 '''
 
+
 #start writing your code below
+base = int(input("Enter the base: "))
+height = int(input("Enter the height: "))
+
+print("The area of the triangle is", base * height / 2)
